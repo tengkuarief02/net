9151f7c04e3feae320bfd0096c293f938e9bd98e
tengkuarief02 <tengkuarief592@gmail.com>
2025-12-30 19:58:04 +0700

    Create theaters.js

---
+++ api/theaters.js
@@ -0,0 +1,6 @@
+export default async function handler(req, res) {
+  const r = await fetch('https://netshort.sansekai.my.id/api/netshort/theaters');
+  const data = await r.json();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).json(data);
+}
